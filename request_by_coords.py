406eb1f937f4931ed99c9cbb63d0a03bc3b2b31d
Zaburskiy A <zaburskiy@gmail.com>
2020-02-15 11:01:37 +0300

    request by coords edited

--- request_by_coords.py
+++ request_by_coords.py
@@ -4,10 +4,11 @@ import requests
 def request(coords):
     size = 0.5
     map_request = \
-        "http://static-maps.yandex.ru/1.x/?ll={}%{}&spn={},{}&l=map".format(coords, size, size)
+        "http://static-maps.yandex.ru/1.x/?ll={},{}&spn={},{}&l=map".format(coords[0], coords[1],
+                                                                            size, size)
     response = requests.get(map_request)
 
-    map_file = "map.png"
+    map_file = "map.jpg"
     with open(map_file, "wb") as file:
         file.write(response.content)
-    return file
+    return map_file
