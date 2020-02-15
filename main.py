00ddd3c226e26ddefc1d12b162d4f16b45d38ff9
mihailmilovidov <mihailmilovidov2004@yandex.ru>
2020-02-15 11:06:34 +0300

    форма на QT

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print()
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+from mapsUI import MapsMainWindow
+
+class Main(QMainWindow, MapsMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.show_map)
+
+    def show_map(self):
+        ll = ",".join([self.lineEdit.text(), self.lineEdit_2.text()])
+        self.pixmap = QPixmap('map.png')
+        self.label_4.setPixmap(self.pixmap)
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
