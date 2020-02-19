40ad2e9708d644d0ca2f1260a6a9208af5ad4610
mihailmilovidov <mihailmilovidov2004@yandex.ru>
2020-02-19 20:07:09 +0300

    Перемещение карты

--- main.py
+++ main.py
@@ -15,20 +15,40 @@ class Main(QMainWindow, MapsMainWindow):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.show_map)
         self.size = 0.5
+        self.searching = True
+
 
     def show_map(self):
-        map_file = request([self.lineEdit.text(), self.lineEdit_2.text()], self.size)
+        map_file = request([self.lineEdit_2.text(), self.lineEdit.text()],
+                           self.size)
         self.pixmap = QPixmap(map_file)
-        print(self.pixmap)
         self.label_4.setPixmap(self.pixmap)
+        print(self.size)
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_PageDown:
+        if event.key() == Qt.Key_PageDown and self.size <= 90 / 1.1:
             self.size *= 1.1
             self.show_map()
-        if event.key() == Qt.Key_PageUp:
+        elif event.key() == Qt.Key_PageUp and self.size >= 0.001:
             self.size *= 0.91
             self.show_map()
+        elif event.key() == Qt.Key_Up:
+            self.lineEdit.setText(
+                str((float(self.lineEdit.text()) + self.size + 90) % 180 - 90))
+            self.show_map()
+        elif event.key() == Qt.Key_Down:
+            self.lineEdit.setText(
+                str((float(self.lineEdit.text()) - self.size + 90) % 180 - 90))
+            self.show_map()
+        elif event.key() == Qt.Key_Right:
+            self.lineEdit_2.setText(str(
+                (float(self.lineEdit_2.text()) + self.size + 180) % 360 - 180))
+            self.show_map()
+        elif event.key() == Qt.Key_Left:
+            self.lineEdit_2.setText(str(
+                (float(self.lineEdit_2.text()) - self.size + 180) % 360 - 180))
+            self.show_map()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
