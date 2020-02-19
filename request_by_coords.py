e9fc6197500a380187fed6556e856ae488e09e54
Egopoler <Egpol2004@yandex.ru>
2020-02-19 19:21:46 +0300

    fun get_coord

--- request_by_coords.py
+++ request_by_coords.py
@@ -11,3 +11,27 @@ def request(coords, size):
     with open(map_file, "wb") as file:
         file.write(response.content)
     return map_file
+
+def get_coord(text):
+    """Получает координаты по адресу объекта
+    text - адресс
+    return x, y"""
+    x, y = None, None
+    toponym_to_find = text
+
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"
+    }
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    if not response:
+        return x, y
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    x, y = toponym_coodrinates.split(" ")
+    return x, y
